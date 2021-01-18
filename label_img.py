b6827fc004eb3df5754e335368a0f2c198feee15
Thomas Diaconu <thomas.diaconu@eurecom.fr>
2021-01-18 23:29:52 +0100

    create main function call

--- label_img.py
+++ label_img.py
@@ -14,17 +14,20 @@ def waste_tag(type):
         return 2
 
 dir = os.listdir(os.path.join(vott_path))
-for k in range(len(dir)):
-    file = open(os.path.join(vott_path, dir[k]),)
-    data = json.load(file)
-    fileName = os.path.join(tags_path, data["asset"]["name"] + ".txt")
-    txtFile =  open(fileName, "w+")
-    for region in data["regions"]:
-        bounding_box = boundingBox(waste_tag(region["tags"][0]), 
-                                    region["boundingBox"]["left"] + region["boundingBox"]["width"]/2, 
-                                    region["boundingBox"]["top"] + region["boundingBox"]["height"]/2 , 
-                                    region["boundingBox"]["width"], 
-                                    region["boundingBox"]["height"])
-        txtFile.write('{} {} {} {} {}'.format(bounding_box.type, bounding_box.x, bounding_box.y, bounding_box.w, bounding_box.h) + "\r\n")
-    
 
+def main():
+    for k in range(len(dir)):
+        file = open(os.path.join(vott_path, dir[k]))
+        data = json.load(file)
+        fileName = os.path.join(tags_path, data["asset"]["name"] + ".txt")
+        txtFile =  open(fileName, "w+")
+        for region in data["regions"]:
+            bounding_box = boundingBox(waste_tag(region["tags"][0]), 
+                                    (region["boundingBox"]["left"] + region["boundingBox"]["width"]/2)/data["asset"]["size"]["width"], 
+                                    (region["boundingBox"]["top"] + region["boundingBox"]["height"]/2)/data["asset"]["size"]["height"], 
+                                    region["boundingBox"]["width"]/data["asset"]["size"]["width"], 
+                                    region["boundingBox"]["height"]/data["asset"]["size"]["height"])
+            txtFile.write('{} {} {} {} {}'.format(bounding_box.type, bounding_box.x, bounding_box.y, bounding_box.w, bounding_box.h) + "\r\n")
+    
+if __name__ == "__main__":
+    main()
